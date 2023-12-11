49c156ffd67660e37a0e3324e8b1428b1410f8c0
Eric Maranga <ekirira22@gmail.com>
2023-12-11 22:16:21 +0300

    completed js assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+const currentUser = "Eric Maranga";
+
+let welcomeMessage = "Welcome to Flatbook, " + `${currentUser}` + "!";
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + `${currentUser.toUpperCase()}` + "!";
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let shortGreeting = "Welcome, " + `${firstInitial}` + "!";
