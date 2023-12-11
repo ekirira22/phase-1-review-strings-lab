7431bb1343818d707fc8b5b7c119e1d93839b26e
Eric Maranga <ekirira22@gmail.com>
2023-12-11 22:26:24 +0300

    adjusted slice() operator to depict last name

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let welcomeMessage = "Welcome to Flatbook, " + `${currentUser}` + "!";
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + `${currentUser.toUpperCase()}` + "!";
 
 const firstInitial = currentUser[0];
-const restOfName = currentUser.slice(1);
+const restOfName = currentUser.slice(5);
 
-let shortGreeting = "Welcome, " + `${firstInitial}` + "!";
+let shortGreeting = "Welcome, " + `${firstInitial} ` + `${restOfName}` + "!";
+
+console.log(shortGreeting);
